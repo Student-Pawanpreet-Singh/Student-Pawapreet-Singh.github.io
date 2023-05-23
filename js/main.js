127d51c062c55a0f9fee9e7a9a52158c6e80a5d0
Student-Pawanpreet-Singh <302485971@student.rocmondriaan.nl>
2023-05-23 11:40:52 +0200

    canvas added + test from js opdracht

--- js/main.js
+++ js/main.js
@@ -24,3 +24,38 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+// Chart Js
+console.log('Main loaded');
+
+const backgroundColours = ['red', 'blue', 'yellow', 'green', 'purple', 'orange'];
+
+const chartExample = document.querySelector('.chart-skill-1');
+
+createChart(chartExample);
+
+function createChart(canvasElement) {
+    new Chart(canvasElement, {
+        type: 'bar',
+        data: {
+            labels: ['T1H', 'T1J', 'T1K', 'T1L', 'T1M', 'T1N'],
+            datasets: [{
+                backgroundColor: backgroundColours,
+                label: '# Aantal studenten per klas',
+                data: [20, 18, 21, 18, 17, 19],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
